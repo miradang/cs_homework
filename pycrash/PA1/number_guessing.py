71e36ec9391e508c5183b443e6367d2fbd1d1200
miradang <mirandadangerfield1066@gmail.com>
2025-11-05 12:03:33 -0500

    finished project 1 other than validation

--- pycrash/PA1/number_guessing.py
+++ pycrash/PA1/number_guessing.py
@@ -7,7 +7,8 @@ print("Number Guessing Game. Choose a upper and lower bound.")
 low = int(input("Lower Bound: "))
 high = int(input("Higher Bound: "))
 
-li_int = list(range(low, high + 1))
+#so i dont make a nested list like li_int = list(range(low, high + 1))
+li_int = [i for i in range(low, high + 1)]
 
 ran_num = random.randrange(low, high)
 
@@ -15,14 +16,14 @@ guess = 0
 guess_total = 0
 
 def binary_search(lst, target):
-    
-    
-    high = len(lst) -1
+    high = len(lst) - 1
     low = 0
-    mid = (high + low) // 2
     num_guess = 0
 
     while low <= high:
+        mid = (high + low) // 2
+        num_guess += 1
+
         if lst[mid] < target:
             low = mid + 1
             num_guess += 1
@@ -30,13 +31,37 @@ def binary_search(lst, target):
         elif lst[mid] > target:
             high = mid - 1
             num_guess += 1
-        else:
+        
+        elif lst[mid] == target:
             return num_guess
+
+        else:
+            pass
+        
     return -1
 
-guess_total = binary_search([li_int], (ran_num))
+guess_total = binary_search(li_int, ran_num)
 
-print(guess_total)
+print(f"You have {guess_total} guesses.")
+a1 = ""
+
+while a1 != ran_num:
+    a1 = int(input("Enter your Guess: "))
+
+    if a1 > ran_num:
+        print("Guess too high")
+        guess_total -= 1
+        print(f"You have {guess_total}s remaining")
+
+    elif a1 < ran_num:
+        print("Guess is too low")
+        guess_total -= 1
+        print(f"You have {guess_total}s remaining")
+
+    else:
+        print("Correct Guess, Hurrah")
+
+    
 
 
 
