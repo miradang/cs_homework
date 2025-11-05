0b8f802fa060b5e6d45f3ff6f1777a4d5397969e
miradang <mirandadangerfield1066@gmail.com>
2025-11-05 05:33:37 -0500

    binary search how to. me not know how index works.

--- pycrash/PA1/number_guessing.py
+++ pycrash/PA1/number_guessing.py
@@ -7,31 +7,34 @@ print("Number Guessing Game. Choose a upper and lower bound.")
 low = int(input("Lower Bound: "))
 high = int(input("Higher Bound: "))
 
-li_int = list(range(low, high))
+li_int = list(range(low, high + 1))
 
 ran_num = random.randrange(low, high)
 
 guess = 0
 guess_total = 0
 
-def binary_search(array, target):
+def binary_search(lst, target):
     
-    high = array[-1]
-    low = array[0]
+    
+    high = len(lst) -1
+    low = 0
     mid = (high + low) // 2
+    num_guess = 0
 
     while low <= high:
-        if array[mid] < target:
+        if lst[mid] < target:
             low = mid + 1
+            num_guess += 1
         
-        elif array[mid] > target:
+        elif lst[mid] > target:
             high = mid - 1
-        
+            num_guess += 1
         else:
-            return mid
+            return num_guess
     return -1
 
-guess_total = binary_search(li_int, ran_num)
+guess_total = binary_search([li_int], (ran_num))
 
 print(guess_total)
 
