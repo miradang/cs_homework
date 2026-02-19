9f782f86918c1d541727063f4cd4f88be52643c1
miradang <mirandadangerfield1066@gmail.com>
2026-02-19 05:16:59 -0500

    man im bad at remebering to commit

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,10 +1,11 @@
 import sys
-
 import pygame
 
 from settings import Settings
-
 from ship import Ship
+from bullet import Bullet
+from alien import Alien
+
 
 class AlienInvasion:
     """
@@ -12,11 +13,7 @@ class AlienInvasion:
     overall class to manage game assesst and behavior
     """
     def __init__(self):
-        """
-        Docstring for __init__
-        
-        :param self: start screen
-        """
+        """Initialize the game, and create game resources."""
         pygame.init()
         self.clock = pygame.time.Clock()
         self.settings = Settings()
@@ -25,22 +22,86 @@ class AlienInvasion:
             (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
-        #set bg color
-        self.bg_color = (230, 230, 230)
+        self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+
+        self._create_fleet()
 
     def run_game(self):
         """"Main game loop"""
 
         while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT():
-                    sys.exit()
-            #redraw screen during each pass thru loop
-            self.screen.fill(self.settings.bg_color)
-
-            #make most recent drawn screen visbile
-            pygame.display.flip()
+            self._check_events()
+            self.ship.update()
+            self.bullets.update()
+            self.remove_bullets()
+            self._update_screen()          
             self.clock.tick(60)
+
+    def _check_events(self):
+        """respond to key strokes and mouse events"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
+
+        #quit the game by pushing p
+        elif event.key == pygame.K_p:
+            sys.exit()
+
+    def _check_keyup_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
+    def _update_screen(self):
+        ''''update images on the screena and flip to the new screen'''
+        #redraw screen during each pass thru loop
+        self.screen.fill(self.settings.bg_color)
+
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
+        
+        self.ship.blitme()
+        self.aliens.draw(self.screen)
+        #make most recent drawn screen visbile
+        pygame.display.flip()
+
+    #def _draw_bullets(self):
+    #    for bullet in self.bullets.sprites():
+    #       bullet.draw_bullet()
+    # figure out how to make that for loop above not awful    ######
+    
+
+    def _fire_bullet(self):
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+    
+    def remove_bullets(self):
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+        #print(len(self.bullets))
+
+    def _create_fleet(self):
+        alien = Alien(self)
+        self.aliens.add(alien)
+
+    
     
 if __name__=='__main__':
     #make game instance and run
--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -1,25 +1,42 @@
 import pygame
 
+
 class Ship:
 
     def __init__(self, ai_game):
-        """
-        Docstring for __init__
-        
-        :param self: initizlie ship
-        :param ai_game: get starting location
-        """
+      
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        #make screen a rect
         self.screen_rect = ai_game.screen.get_rect()
 
         #load ship images and get rect
         self.image = pygame.image.load('images/ship.bmp')
-        self.rect = self.images.get_rect()
+        self.rect = self.image.get_rect()
 
         self.rect.midbottom = self.screen_rect.midbottom
 
-    def blitme(self):
+        self.x = float(self.rect.x)
 
+
+        self.moving_right = False
+        self.moving_left = False
+
+
+    def update(self):
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+            
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+        
+        self.rect.x = self.x
+
+    def blitme(self):
+       
         self.screen.blit(self.image, self.rect)
+
+
+
 
         
