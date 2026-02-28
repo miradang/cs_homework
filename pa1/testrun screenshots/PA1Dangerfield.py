5f7990d607ccd4e951b7a1175a2cd8a5b9c81386
miradang <mirandadangerfield1066@gmail.com>
2026-02-27 22:35:28 -0500

    nm mnb

--- pa1/testrun screenshots/PA1Dangerfield.py
+++ pa1/testrun screenshots/PA1Dangerfield.py
@@ -9,25 +9,12 @@
 
 import math
 
-quit = 0
 
-while quit == 0:
+#me being angry i have to put the function at the top of the program
 
-    print("This program will tell you if a fraction is proper or improper.\nType q to quit")
-#figure out if you should convert to int or just store num/den as int for the quit condition
-    num_a = int(input("Enter a numerator: "))
-    if num_a == "q":
-        break
-    den_a = int(input("Enter a denominator:  "))
-    if den_a == "q":
-        break
-    
-    proper_fraction(num_a, den_a)
-
-           
 def proper_fraction(num, den):
     if den == 0:
-        print("The denominator cannot be zero")
+        print("The denominator cannot be zero.")
         return
     
     else:
@@ -35,7 +22,52 @@ def proper_fraction(num, den):
         abs_den = abs(den)
         
         if abs_num < abs_den:
-            print(f"{abs(num)} / {abs(den)} is a proper fraction")
+            print(f"{abs(num)} / {abs(den)} is a proper fraction.")
+            return
+        
+        else:
+            whole = num // den
+            remain = abs(num % den)
+
+            if remain == 0:
+                print(f"{num} / {den} is an improper fraction and it can be reduced to {whole}")
+                #output negative improper fractions show as -(a + b/c)
+                return
+        if remain != 0:
+            divisor = math.gcd(remain, den)
+            remain //= divisor
+            simplified_den = den // divisor
+        
+        else:
+            simplified_den = den
+
+        if num < 0:
+            print(f"{num} / {den} is an improper fraction and its mixed fraction is -({abs(whole)} + {remain} / {simplified_den}).")
+            return
+        else:
+            print(f"{num} / {den} is an improper fraction and its mixed fraction is {whole} + {remain} / {simplified_den}.")
+            return
+
+#quit condition
+quit = 0
+
+#main program loop
+while quit == 0:
+
+    print("\nThis program will tell you if a fraction is proper or improper.\n\tType q to quit.\n")
+#figure out if you should convert to int or just store num/den as int for the quit condition
+    num_a = int(input("Enter a numerator: "))
+    if num_a == "q":
+        break
+    den_a = int(input("Enter a denominator: "))
+    if den_a == "q":
+        break
+    
+    #calls the proper fraction function above
+    proper_fraction(num_a, den_a)
+
+           
+
 
 
 
---
+++ pa1/testrun screenshots/import math.py
@@ -0,0 +1,7 @@
+import math
+
+num = int(input("numerator: "))
+den = int(input("denominator: "))
+
+c = num % den
+print(c)
